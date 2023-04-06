cba247ff8ba5172943cca7563918ef1831fefdfa
MrSeeWinTer <mrseewinter@gmail.com>
2023-04-06 17:36:44 +0800

    UpdateNav

--- script.js
+++ script.js
@@ -45,86 +45,97 @@ function getSelectedValues() {
 }
 
 
-let cleantable = 0;
+
 function fetchData() {
-  const existingTable = document.querySelector('table.my-table');
-  if (existingTable) {
-    const table = document.getElementsByClassName('my-table')[cleantable];
-    table.innerHTML = '';
-    cleantable++;
-  } else {
-    // The element does not have the class "my-class"
-  }
+    const existingTable = document.getElementById('table');
+    if (existingTable) {
+      const table = document.getElementById('table');
+      table.innerHTML = '';
+
+      const geticon = document.getElementById('logo');
+      geticon.innerHTML = '';
+    } else {
+      // The element does not have the class "my-class"
+    }
 
 
-  const { itemName, itemTier, itemEnchant, itemQuality } = getSelectedValues();
+    const { itemName, itemTier, itemEnchant, itemQuality } = getSelectedValues();
 
-  const url = `https://east.albion-online-data.com/api/v2/stats/Prices/${itemTier}_${itemName}${itemEnchant}.json${itemQuality}`; 
+    const logoDiv = document.getElementById('logo');
+    const logoImg = document.createElement('img');
+    logoImg.src = `https://render.albiononline.com/v1/item/${itemTier}_${itemName}${itemEnchant}.png`;
+    logoDiv.appendChild(logoImg);
 
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const table = document.createElement('table');
-    table.classList.add('my-table');
-    
-    let headerRow = document.createElement('tr');
-    let headers = ['City', 'Sell Price', 'Sell Price Date','Buy Price','Buy Price Date'];
+    const url = `https://east.albion-online-data.com/api/v2/stats/Prices/${itemTier}_${itemName}${itemEnchant}.json${itemQuality}`; 
 
-    headers.forEach(header => {
-      let th = document.createElement('th');
-      th.textContent = header;
-      headerRow.appendChild(th);
-    });
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const table = document.createElement('table');
+      table.classList.add('my-table');
+      
+      let headerRow = document.createElement('tr');
+      let headers = ['City', 'Sell Price', 'Sell Price Date','Buy Price','Buy Price Date'];
 
-    table.appendChild(headerRow);
-
-    let cities = data.map(item => item.city).filter((value, index, self) => self.indexOf(value) === index);
-
-    cities.forEach(city => {
-      let row = document.createElement('tr');
-      row.setAttribute('data-city', city);
-      let cityNameCell = document.createElement('td');
-      cityNameCell.textContent = city;
-
-      row.appendChild(cityNameCell);
-
-      let cityItems = data.filter(item => item.city === city);
-
-      headers.slice(1).forEach(header => {
-        let cell = document.createElement('td');
-        let price;
-        let date;
-
-        if (header === 'Sell Price') {
-          price = Math.min(...cityItems.map(item => item.sell_price_min));
-        } else if (header === 'Sell Price Date') {
-          date = cityItems.map(item => item.sell_price_min_date);
-        } else if (header === 'Buy Price') {
-          price = Math.max(...cityItems.map(item => item.buy_price_max));
-        } else if (header === 'Buy Price Date') {
-          date = cityItems.map(item => item.buy_price_max_date);
-        }
-        //date = new Date(date).toLocaleString()
-        const utcDate = new Date(date);
-        const offset = 8; // UTC+8 timezone offset
-        const localTimestamp = new Date(utcDate.getTime() + (offset * 60 * 60 * 1000));
-        const time = localTimestamp.toLocaleString('en-US', { month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false });
-
-
-        cell.textContent = header.includes('Date') ?   (time!="1/1, 08:00")?time: "NO DATA" : price;
-
-        row.appendChild(cell);
+      headers.forEach(header => {
+        let th = document.createElement('th');
+        th.textContent = header;
+        headerRow.appendChild(th);
       });
 
-      table.appendChild(row);
-    });
+      table.appendChild(headerRow);
 
-    document.body.appendChild(table);
-  })
+      let cities = data.map(item => item.city).filter((value, index, self) => self.indexOf(value) === index);
+
+      cities.forEach(city => {
+        let row = document.createElement('tr');
+        row.setAttribute('data-city', city);
+        let cityNameCell = document.createElement('td');
+        cityNameCell.textContent = city;
+
+        row.appendChild(cityNameCell);
+
+        let cityItems = data.filter(item => item.city === city);
+
+        headers.slice(1).forEach(header => {
+          let cell = document.createElement('td');
+          let price;
+          let date;
+
+          if (header === 'Sell Price') {
+            price = Math.min(...cityItems.map(item => item.sell_price_min));
+          } else if (header === 'Sell Price Date') {
+            date = cityItems.map(item => item.sell_price_min_date);
+          } else if (header === 'Buy Price') {
+            price = Math.max(...cityItems.map(item => item.buy_price_max));
+          } else if (header === 'Buy Price Date') {
+            date = cityItems.map(item => item.buy_price_max_date);
+          }
+          //date = new Date(date).toLocaleString()
+          const utcDate = new Date(date);
+          const offset = 8; // UTC+8 timezone offset
+          const localTimestamp = new Date(utcDate.getTime() + (offset * 60 * 60 * 1000));
+          const time = localTimestamp.toLocaleString('en-US', { month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false });
+
+
+          cell.textContent = header.includes('Date') ?   (time!="1/1, 08:00")?time: "NO DATA" : price;
+
+          row.appendChild(cell);
+        });
+
+        table.appendChild(row);
+      });
+      
+      const tablediv = document.getElementById('table');
+      tablediv.appendChild(table);
+    })
         .catch(error => {
           const pricesDiv = document.getElementById("prices");
           pricesDiv.textContent = "Error fetching data from API.";
         });
+
+
+
       }
 
 
