8cd0e9c30f620ff95ccfa5cbae351071849882f8
MrSeeWinTer <mrseewinter@gmail.com>
2023-04-08 17:31:28 +0800

    UpdatePlayer&Guild

--- guild/script.js
+++ guild/script.js
@@ -2,23 +2,47 @@
 
 
 function fetchData() {
-    const usernameInput = document.getElementById("username");
-    const url1 = "https://gameinfo-sgp.albiononline.com/api/gameinfo/search?q=SeeWinTer.";
+    const guildnameInput = document.getElementById("guildname");
+    const url1 = "https://gameinfo-sgp.albiononline.com/api/gameinfo/search?q="+guildnameInput.value;
     const url2 = "https://www.example.com.";
     
-    console.log(usernameInput.value);
+    const guilddatacontainer = document.getElementById('guildData');
+
+    if (guilddatacontainer) {
+        document.body.removeChild(guilddatacontainer);
+    }
+
+    //console.log(url1);
     
-    fetch(url2,{headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "https://www.example.com",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        }},)
+    fetch(url1)
     .then(response => response.json())
     .then(data => {
         console.log(data);
+
+        const guildData = data.guilds.filter(p => p.Name === guildnameInput.value)[0];
+
+        //console.log(player);
+        const guilddatacontainer = document.createElement('div');
+        guilddatacontainer.setAttribute('id', 'guildData');
+
+        
+        const display_guilddata = `<h2>公會資料</h2>
+        <p><strong>公會名稱:</strong> ${guildData.Name}</p>
+        <p><strong>同盟名稱:</strong> ${guildData.AllianceName}</p>
+        <p><strong>擊殺名譽:</strong> ${guildData.KillFame}</p>
+        <p><strong>死亡名譽:</strong> ${guildData.DeathFame}</p>
+        `;
+
+
+       guilddatacontainer.innerHTML = display_guilddata;
+       document.body.appendChild(guilddatacontainer);
+       
     })
     .catch(error => {
-        console.log(error);
+        const guilddatacontainer = document.createElement('div');
+        guilddatacontainer.setAttribute('id', 'guildData');
+        guilddatacontainer.innerHTML = "沒有此公會";
+        document.body.appendChild(guilddatacontainer);
     });
 
     /*
--- player/script.js
+++ player/script.js
@@ -3,18 +3,48 @@
 
 function fetchData() {
     const usernameInput = document.getElementById("username");
-    const url1 = "https://gameinfo-sgp.albiononline.com/api/gameinfo/search?q=SeeWinTer.";
+    const url1 = "https://gameinfo-sgp.albiononline.com/api/gameinfo/search?q="+usernameInput.value;
     const url2 = "https://www.example.com.";
     
-    console.log(usernameInput.value);
-    
+    //console.log(url1);
+    const playerdatacontainer = document.getElementById('playerData');
+
+    if (playerdatacontainer) {
+        document.body.removeChild(playerdatacontainer);
+    }
+
     fetch(url1)
     .then(response => response.json())
     .then(data => {
         console.log(data);
+
+        const playerData = data.players.filter(p => p.Name === usernameInput.value)[0];
+
+        //console.log(player);
+        const playerdatacontainer = document.createElement('div');
+        playerdatacontainer.setAttribute('id', 'playerData');
+
+        //let playerdatacontainer = document.getElementById('playerData');
+
+        const display_playerdata = `<h2>玩家資料</h2>
+        <p><strong>玩家名稱:</strong> ${playerData.Name}</p>
+        <p><strong>公會名稱:</strong> ${playerData.GuildName}</p>
+        <p><strong>擊殺名譽:</strong> ${playerData.KillFame}</p>
+        <p><strong>死亡名譽:</strong> ${playerData.DeathFame}</p>
+        <p><strong>名譽比例:</strong> ${playerData.FameRatio}</p>
+        <p><strong>總擊殺數:</strong> ${playerData.totalKills}</p>
+      `;
+
+
+        playerdatacontainer.innerHTML = display_playerdata;
+        document.body.appendChild(playerdatacontainer);
+       
     })
     .catch(error => {
-        console.log(error);
+        const playerdatacontainer = document.createElement('div');
+        playerdatacontainer.setAttribute('id', 'playerData');
+        playerdatacontainer.innerHTML = "沒有此玩家";
+        document.body.appendChild(playerdatacontainer);
     });
 
     /*
